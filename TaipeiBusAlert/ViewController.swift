c735f1653d00c31fd075058a3fff49048861359d
Wayne Yeh <wayne.yeh@eten.com.tw>
2018-02-09 10:42:34 +0800

    Initial Commit

---
+++ TaipeiBusAlert/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TaipeiBusAlert
+//
+//  Created by Wayne Yeh on 2018/2/9.
+//  Copyright © 2018年 Wayne Yeh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
